b368b26980388d06b980b9d9481b618578c5eb9f
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-04-03 22:00:44 +0530

    (Refactor): ProductModel to a class structure, implementing a constructor and static fromJson method for improved instantiation and mapping from JSON data.

--- src/core/home/data/model/productModel.ts
+++ src/core/home/data/model/productModel.ts
@@ -1,27 +1,49 @@
 import { ProductEntity } from "../../domain/entity/productEntity";
 
-export interface ProductModel {
+export class ProductModel {
   id: number;
   title: string;
   description: string;
   price: number;
   thumbnail: string;
+
+  constructor({
+    id,
+    title,
+    description,
+    price,
+    thumbnail,
+  }: {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    thumbnail: string;
+  }) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.price = price;
+    this.thumbnail = thumbnail;
+  }
+
+  static fromJson(json: Record<string, unknown>): ProductModel {
+    return new ProductModel({
+      id: json.id as number,
+      title: json.title as string,
+      description: json.description as string,
+      price: json.price as number,
+      thumbnail: json.thumbnail as string,
+    });
+  }
+
+  toEntity(): ProductEntity {
+    return {
+      id: this.id,
+      name: this.title,
+      description: this.description,
+      price: this.price,
+      thumbnail: this.thumbnail,
+    };
+  }
 }
-
-export const ProductModelMapper = {
-  fromJson: (json: any): ProductModel => ({
-    id: json.id,
-    title: json.title,
-    description: json.description,
-    price: json.price,
-    thumbnail: json.thumbnail,
-  }),
-
-  toEntity: (model: ProductModel): ProductEntity => ({
-    id: model.id,
-    name: model.title,
-    description: model.description,
-    price: model.price,
-    thumbnail: model.thumbnail,
-  }),
-};
