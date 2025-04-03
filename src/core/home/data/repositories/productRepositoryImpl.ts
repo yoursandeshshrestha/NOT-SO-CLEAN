49bf9be7b9c0051650ad5e06e7c3155fbdef1c1a
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-04-03 22:01:05 +0530

    (Refactor): ProductRepositoryImpl to remove ProductModelMapper and directly map API data to ProductEntity using toEntity method.

--- src/core/home/data/repositories/productRepositoryImpl.ts
+++ src/core/home/data/repositories/productRepositoryImpl.ts
@@ -1,6 +1,5 @@
 import { ProductRepository } from "../../domain/repositories/productRepository";
 import { ProductEntity } from "../../domain/entity/productEntity";
-import { ProductModelMapper } from "../model/productModel";
 import { ProductApiDataSource } from "../datasource/productApiDataSource.ts";
 
 export class ProductRepositoryImpl implements ProductRepository {
@@ -8,11 +7,11 @@ export class ProductRepositoryImpl implements ProductRepository {
 
   async getProducts(): Promise<ProductEntity[]> {
     const products = await this.productApi.getProducts();
-    return products.map((p) => ProductModelMapper.toEntity(p));
+    return products.map((p) => p.toEntity());
   }
 
   async getProductById(id: number): Promise<ProductEntity> {
     const product = await this.productApi.getProductById(id);
-    return ProductModelMapper.toEntity(product);
+    return product.toEntity();
   }
 }
